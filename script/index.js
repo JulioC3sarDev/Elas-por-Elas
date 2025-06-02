61127c5186a2973072de59133c4f9ca1d2543ec4
Júlio César <kaezajulius.jc@gmail.com>
2025-06-02 11:36:50 -0300

    Animações 2.0

--- script/index.js
+++ script/index.js
@@ -1,4 +1,19 @@
+const intro = document.querySelectorAll(".resumo")
+
+const obs2 = new IntersectionObserver ((ol) => {
+    console.log(ol);
+    ol.forEach((entry2) => {
+        if(entry2.isIntersecting){
+            entry2.target.classList.add('show');
+        }
+    });
+})
+
+intro.forEach((element) => obs2.observe(element) )
+
+
 const sec = document.querySelectorAll(".topicos-principal")
+
 
 const obs = new IntersectionObserver ((al) => {
     al.forEach((entry) => {
@@ -12,3 +27,4 @@ const obs = new IntersectionObserver ((al) => {
 })
 
 sec.forEach((element) => obs.observe(element) )
+
